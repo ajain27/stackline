37c5db5234932648f9e0318f4ad0161300348e91
v-jaia <v-jaia@microsoft.com>
2020-04-04 15:43:47 -0700

    Implementing Chart Data

--- stackline/src/app/app.component.ts
+++ stackline/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StacklineService } from './stackline.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,12 @@ import { StacklineService } from './stackline.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  chart = [];
+  public months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC' ];
   public stackLineData: any;
   public salesData: any;
-  public weelSales: number;
+  public weekEnding: number;
   public retailSales: number;
   public wholesaleSales: number;
   public unitsSold: number;
@@ -19,9 +23,49 @@ export class AppComponent implements OnInit {
 
   public ngOnInit() {
     this.stackLineService.getStackLineData().subscribe((res) => {
-      this.stackLineData = res;
+      this.stackLineData = res ? res : null;
       for (const data of this.stackLineData) {
-        this.salesData = data.sales;
+        console.log(data);
+        this.salesData = data && data.sales ? data.sales : null;
+
+        // Chart code
+
+        // tslint:disable-next-line: no-string-literal
+        this.retailSales = data['sales'].map((sales: any) => sales.retailSales);
+        // tslint:disable-next-line: no-string-literal
+        this.wholesaleSales = data['sales'].map((sales: any) => sales.wholesaleSales);
+
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: this.months,
+            datasets: [
+              {
+                data : this.retailSales,
+                borderColor: '#03a5fc',
+                fil: false
+              },
+              {
+                data : this.wholesaleSales,
+                borderColor: '#becacf',
+                fil: false
+              }
+            ]
+          },
+          options:  {
+            legen: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }]
+            }
+          }
+        });
       }
     });
   }
