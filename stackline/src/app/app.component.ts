b1334c621376e11e08c178e9344b978517015e73
v-jaia <v-jaia@microsoft.com>
2020-04-04 17:03:23 -0700

    Adding the box shadow

--- stackline/src/app/app.component.ts
+++ stackline/src/app/app.component.ts
@@ -9,17 +9,14 @@ import { Chart } from 'chart.js';
 })
 export class AppComponent implements OnInit {
 
-  chart = [];
   public months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC' ];
   public stackLineData: any;
   public salesData: any;
-  public weekEnding: number;
   public retailSales: number;
   public wholesaleSales: number;
-  public unitsSold: number;
-  constructor(
-    private stackLineService: StacklineService
-  ) { }
+  public chart = [];
+
+  constructor(private stackLineService: StacklineService) { }
 
   public ngOnInit() {
     this.stackLineService.getStackLineData().subscribe((stackLineResponse) => {
