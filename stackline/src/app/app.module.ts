1d5c8d9a8429b7e8ebf54fcd5aa0737e68b00520
v-jaia <v-jaia@microsoft.com>
2020-04-08 15:18:10 -0700

    Refactoring the code to make it more modular

--- stackline/src/app/app.module.ts
+++ stackline/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule, routingConstants } from './app.routing.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SalesComponent } from './components/sales/sales.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,15 +11,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    routingConstants,
     SidebarComponent,
     SalesComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
